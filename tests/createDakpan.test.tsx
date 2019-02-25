9e648a8c2529ca424c19f36da5f138893a5e3a9a
houfio <lexhoufio@gmail.com>
2019-02-25 13:13:12 +0100

    Add tests back

---
+++ tests/createDakpan.test.tsx
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import { fireEvent, render, waitForDomChange } from 'react-testing-library';
+
+import { createDakpan } from '../src/createDakpan';
+
+const [CountProvider, useCount] = createDakpan({
+  count: 1
+})({
+  increase: () => ({ count }) => ({
+    count: count + 1
+  })
+});
+
+function Counter() {
+  const [countState, countActions] = useCount();
+
+  return (
+    <button data-testid="button" onClick={countActions.increase}>{countState.count}</button>
+  );
+}
+
+function App() {
+  return (
+    <CountProvider>
+      <Counter/>
+    </CountProvider>
+  );
+}
+
+it('should return an array with a provider and hook', () => {
+  expect(CountProvider).toBeDefined();
+  expect(useCount).toBeDefined();
+});
+
+it('should throw when the provider isn\'t mounted', () => {
+  expect(() => render(<Counter/>)).toThrow('Provider not mounted');
+});
+
+it('should provide the initial state', () => {
+  const { getByTestId } = render(<App/>);
+  const button = getByTestId('button');
+
+  expect(button.textContent).toEqual('1');
+});
+
+it('should update the state', async () => {
+  const { getByTestId } = render(<App/>);
+  const button = getByTestId('button');
+
+  fireEvent.click(button);
+
+  await waitForDomChange();
+
+  expect(button.textContent).toEqual('2');
+});
